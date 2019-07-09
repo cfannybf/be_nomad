02dad594fa2b0b63f4b494bc7e3a82e0ac7ed951
cfannybf <hobbitbf2@gmail.com>
2019-07-09 20:20:08 +0200

    Feature/front end (#9)
    * Front End Website. Mockup structure. Base transitions. Color Scheme.
    
    * Front-end folder structure.
    
    * Vue Project
    
    * Component structure. Reactive website.

---
+++ webapp/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <FrontPage />
+    <SearchResults />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import FrontPage from "./components/FrontPage.vue";
+import SearchResults from "./components/SearchResults.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+
+export default {
+  name: "app",
+  components: {
+    NavBar,
+    FrontPage,
+    SearchResults,
+    FooterComponent
+  }
+};
+</script>
+
+<style>
+nav ul a,
+nav .brand-logo {
+  color: #e8eaf6;
+}
+
+p {
+  line-height: 2rem;
+}
+
+.sidenav-trigger {
+  color: #26a69a;
+}
+
+.tags {
+  font-weight: bold;
+  font-family: monospace;
+  padding-top: 20px;
+}
+
+.parallax-container {
+  min-height: 847px;
+  line-height: 0;
+  height: auto;
+  color: rgba(255, 255, 255, 0.9);
+}
+.parallax-container .section {
+  width: 100%;
+  padding-top: 10%;
+}
+
+.image {
+  opacity: 1 !important;
+}
+
+@media only screen and (max-width: 992px) {
+  .parallax-container .section {
+    position: absolute;
+    top: 1%;
+  }
+  #index-banner .section {
+    top: 1%;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  #index-banner .section {
+    top: 0;
+  }
+}
+
+.icon-block {
+  padding: 0 15px;
+}
+.icon-block .material-icons {
+  font-size: inherit;
+}
+
+footer.page-footer {
+  margin: 0;
+}
+</style>
---
+++ webapp/src/components/SearchResults.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="searchresults">
+    <div class="container" id="search_result">
+      <div class="row">
+        <h5 class="center">Search results</h5>
+      </div>
+
+      <div class="row">
+        <div class="col s4" v-for="n in 7">
+          <div class="card light-blue lighten-4">
+            <div class="card-content grey-text text-darken-1">
+              <span class="card-title">Job title</span>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dolor erat, bibendum
+                sed tortor nec,
+                consequat sollicitudin ex.
+              </p>
+              <p class="tags">Lorem Ipsum# Dolor++ Sit Amet</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchResults"
+};
+</script>
